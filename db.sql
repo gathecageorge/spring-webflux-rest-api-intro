c13fc06f7057cdf867ef12e895aaa35f02a78e58
George <gatheca.george@gmail.com>
2021-10-16 13:28:32 +0100

    Rearrange db.sql commands for creating database

--- db.sql
+++ db.sql
@@ -5,18 +5,6 @@ CREATE TABLE `courses` (
   UNIQUE KEY `coursename` (`coursename`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `coursework` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `student_id` int(10) unsigned NOT NULL,
-  `course_id` int(10) unsigned NOT NULL,
-  `marks` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `student_id` (`student_id`),
-  KEY `course_id` (`course_id`),
-  CONSTRAINT `coursework_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`),
-  CONSTRAINT `coursework_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 CREATE TABLE `students` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
@@ -28,3 +16,15 @@ CREATE TABLE `students` (
 insert  into `students`(`id`,`name`,`registered_on`,`status`) values
 (1,'George Gatheca Updated',1234,1),
 (2,'Jonh Doe',5678,1);
+
+CREATE TABLE `coursework` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `student_id` int(10) unsigned NOT NULL,
+  `course_id` int(10) unsigned NOT NULL,
+  `marks` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `student_id` (`student_id`),
+  KEY `course_id` (`course_id`),
+  CONSTRAINT `coursework_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`),
+  CONSTRAINT `coursework_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
